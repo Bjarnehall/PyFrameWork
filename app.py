555cebeeccc18a77af9ebfa58364936ad6256447
Bjarnehall <Jonas.bjarnehall@outlook.com>
2024-09-26 19:54:54 +0200

    edit code

--- app.py
+++ app.py
@@ -2,23 +2,23 @@ from api import API
 
 app = API()
 
+# Makes is possible to render template
 def render_template(template, **kwargs):
-    """Simple template rendering function."""
     for key, value in kwargs.items():
         template = template.replace(f"{{{{ {key} }}}}", value)
     return template
 
+
+# Home takes a html template and uses information from functions.py
 @app.route("/home")
 def home(request, response):
     with open("home.html", "r") as file:
         template = file.read()
     
-    # Call your function to get the message
     greeting = greet_from_python()
     
-    # Render the template with the greeting
     response.text = render_template(template, greet_from_python=greeting)
-    response.content_type = "text/html"  # Set the content type to HTML
+    response.content_type = "text/html"
 
 @app.route("/about")
 def about(request,response):
