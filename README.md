3a4ad59d80d345636791027f912bdf606b40d540
ROLAND <roland@Air-de-Malika.lan>
2024-09-28 08:50:34 +0200

    Files

---
+++ README.md
@@ -0,0 +1,6 @@
+# PyFrameWork
+Python framework
+
+# Test for practise #
+
+# Pinging back to you #
---
+++ app.py
@@ -0,0 +1,26 @@
+from functions import greet_from_python
+from api import API
+
+app = API()
+
+# Makes is possible to render template
+def render_template(template, **kwargs):
+    for key, value in kwargs.items():
+        template = template.replace(f"{{{{ {key} }}}}", value)
+    return template
+
+
+# Home takes a html template and uses information from functions.py
+@app.route("/")
+def home(request, response):
+    with open("home.html", "r") as file:
+        template = file.read()
+
+    greeting = greet_from_python()
+    
+    response.text = render_template(template, greet_from_python=greeting)
+    response.content_type = "text/html"
+
+@app.route("/about")
+def about(request,response):
+    response.text = "Hello from about!"
