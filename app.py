81a9fb05cacd0300be81cda33abe2b52b92e7c84
ROLAND <roland@Air-de-Malika.lan>
2024-09-28 19:49:28 +0200

    Added dynamic routes

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
-from functions import greet_from_python, snake_menu
+from functions import greet_from_python
 from api import API
 
 app = API()
+
+# All menu pages go here
+menu_items = [
+    {
+        "name": "Home",
+        "route": "/"
+    },
+    {
+        "name": "About",
+        "route": "/about"
+    },
+    {
+        "name": "Contact",
+        "route": "/contact"
+    }
+]
 
 # Makes is possible to render template
 def render_template(template, **kwargs):
@@ -9,23 +25,29 @@ def render_template(template, **kwargs):
         template = template.replace(f"{{{{ {key} }}}}", value)
     return template
 
+def snake_menu():
+    menu = "<ul>"
+    for item in menu_items:
+        menu += f"<li><a href='{item['route']}'>{item['name']}</a></li>"
+    menu += "</ul>"
+    return menu
 
-# Home takes a html template and uses information from functions.py
-@app.route("/")
-def home(request, response):
-    with open("home.html", "r") as file:
-        template = file.read()
+def create_routes(app, menu_items):
+    for item in menu_items:
+        route = item["route"]
+        name = item["name"].lower()
 
-    greeting = greet_from_python()
-    nav_menu = snake_menu()
-    
-    response.text = render_template(template, greet_from_python=greeting, snake_menu=nav_menu)
-    response.content_type = "text/html"
+        @app.route(route)
+        def dynamic_route(request, response, name=name):
+            try:
+                with open(f"{name}.html", "r") as file:
+                    template = file.read()
+                greeting = greet_from_python()
+                nav_menu = snake_menu()
+                response.text = render_template(template, greet_from_python=greeting, snake_menu=nav_menu)
+                response.content_type = "text/html"
+            except FileNotFoundError:
+                response.text = f"Hello from {name.lower()}!"
+                response.content_type = "text/html"
 
-@app.route("/About")
-def about(request,response):
-    response.text = "Hello from about!"
-
-@app.route("/Contact")
-def contact(request,response):
-    response.text = "Hello from contact!"
+create_routes(app, menu_items)
